1737551786a1762a0e4f8b05332a7c37dc8a332c
gouni <gounimahesh9000@gmail.com>
2024-11-26 20:03:36 +0530

    af

---
+++ src/main/java/com/practices/perfect/PerfectApplication.java
@@ -0,0 +1,13 @@
+package com.practices.perfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practices/perfect/PerfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices.perfect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
